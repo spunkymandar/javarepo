4bafa4844552f1641d94cb2cb6278fb328d8673f
spunkymandar <59153449+spunkymandar@users.noreply.github.com>
2024-03-20 10:07:57 +0530

    Update App.java

--- Project1/src/main/java/com/data/App.java
+++ Project1/src/main/java/com/data/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World..this is 2024! March" );
+        System.out.println( "Hello World..this is 2024! change1" );
     }
 }
