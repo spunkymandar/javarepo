c6c89f300a5af381b896d5367f813b25a3685c23
spunkymandar <59153449+spunkymandar@users.noreply.github.com>
2024-03-24 09:27:47 +0530

    Update App.java
    new function added

--- Code_Cove_Demo/src/main/java/com/data/App.java
+++ Code_Cove_Demo/src/main/java/com/data/App.java
@@ -10,4 +10,7 @@ public class App
     {
         System.out.println( "Hello World..this is 2024! change123 using in eclipse" );
     }
+    public int addNumbers(int no1,int no2){
+        return no1+no2;
+    }
 }
