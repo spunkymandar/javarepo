14fdeb9f59f338d9412fb7987d2d48022bcc290d
spunkymandar <59153449+spunkymandar@users.noreply.github.com>
2024-03-24 09:16:46 +0530

    code coverage

---
+++ Code_Cove_Demo/src/main/java/com/data/App.java
@@ -0,0 +1,13 @@
+package com.data;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World..this is 2024! change123 using in eclipse" );
+    }
+}
