693d2608f70d81c64c8c2a3631a55b2d75e6e505
spunkymandar <59153449+spunkymandar@users.noreply.github.com>
2024-04-13 14:37:28 +0530

    Update App.java

--- Project1/src/main/java/com/data/App.java
+++ Project1/src/main/java/com/data/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World..this is 2024! change123 using in the latest eclipse" );
+        System.out.println( "Hello World..this is 2024! change123 using in the latest eclipse in JNEC" );
     }
 }
